f346e2fe8f43d98ffd5f4e86dec7831c27bcec89
snoe925 <snoe925@gmail.com>
2017-04-13 09:04:37 -0500

    Change path to API header to sass/context.h
    Upstream repository refactored the location of the API headers.

--- generic/tclsass.c
+++ generic/tclsass.c
@@ -10,7 +10,7 @@
 #include <stdlib.h>		/* NOTE: For free(). */
 #include <string.h>		/* NOTE: For strlen(), strcmp(), strdup(). */
 #include "tcl.h"		/* NOTE: For public Tcl API. */
-#include "sass_context.h"	/* NOTE: For public libsass API. */
+#include "sass/context.h"	/* NOTE: For public libsass API. */
 #include "pkgVersion.h"		/* NOTE: Package version information. */
 #include "tclsassInt.h"		/* NOTE: For private package API. */
 #include "tclsass.h"		/* NOTE: For public package API. */
